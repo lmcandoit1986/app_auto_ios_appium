02ab9d4c9a5a720e0b1fbdde39685a2f7d329dda
lmcandoit1986 <lmcandoit@126.com>
2020-04-15 16:37:52 +0800

    1、增加解锁的操作 默认图案：7

--- Page/MainPage.py
+++ Page/MainPage.py
@@ -2,13 +2,15 @@
 # -*- coding: utf-8 -*-
 import os
 import sys
+
+from appium.webdriver.common.touch_action import TouchAction
 from selenium.webdriver.common.by import By
 
 curPath = os.path.abspath(os.path.dirname(__file__))
 rootPath = os.path.split(curPath)[0]
 sys.path.append(rootPath)
 
-from Utils import Operate, Asserts
+from Utils import Operate, Asserts, Support
 from Utils.Dec import elementDecorator
 from Page.BeforPage import BeforePage
 
@@ -23,11 +25,27 @@ from Page.BeforPage import BeforePage
 
 class MainPage:
 
+    tap_1 = '//XCUIElementTypeApplication[@name="华能成长宝"]/XCUIElementTypeWindow[1]/XCUIElementTypeOther[2]/XCUIElementTypeOther/XCUIElementTypeOther[2]/XCUIElementTypeOther[1]'
+    tap_2 = '//XCUIElementTypeApplication[@name="华能成长宝"]/XCUIElementTypeWindow[1]/XCUIElementTypeOther[2]/XCUIElementTypeOther/XCUIElementTypeOther[2]/XCUIElementTypeOther[2]'
+    tap_3 = '//XCUIElementTypeApplication[@name="华能成长宝"]/XCUIElementTypeWindow[1]/XCUIElementTypeOther[2]/XCUIElementTypeOther/XCUIElementTypeOther[2]/XCUIElementTypeOther[4]'
+
     def __init__(self, driver):
         self.driver = driver
 
     @elementDecorator(By.NAME, "保险")
     def _objectBack(self):
+        pass
+
+    @elementDecorator(By.XPATH, tap_1)
+    def _object1(self):
+        pass
+
+    @elementDecorator(By.XPATH, tap_2)
+    def _object2(self):
+        pass
+
+    @elementDecorator(By.XPATH, tap_3)
+    def _object3(self):
         pass
 
     @elementDecorator(By.ID, "id_tile")
@@ -43,3 +61,21 @@ class MainPage:
 
     def assertTitle(self):
         Asserts.assertTrueNoPic(False, "reason")
+
+    def actionUnlock(self):
+        tap1_rect = self._object1().rect
+        tap2_rect = self._object2().rect
+        tap3_rect = self._object3().rect
+        x = tap1_rect.get('x')+tap1_rect.get('width')/2
+        y = tap1_rect.get('y')+tap1_rect.get('height')/2
+        tox = tap2_rect.get('x')-tap1_rect.get('x')
+        toy = tap3_rect.get('y')-tap1_rect.get('y')
+        TouchAction(driver=self.driver).press(None, x, y).wait(300)\
+            .move_to(None, x+tox, y).wait(300)\
+            .move_to(None, x+tox+tox, y).wait(300)\
+            .move_to(None, x+tox+tox, y+toy).wait(300)\
+            .move_to(None, x+tox+tox, y+toy+toy)\
+            .release().perform()
+        Support.sleep(10)
+        return self
+
